459eec0a70db8fc1576695f7ecb30fb785fba2d0
qxuwll <ivanskarupo23@gmail.com>
2025-08-21 22:56:08 +0700

    Add files via upload

--- src/simplejsoner/__init__.py
+++ src/simplejsoner/__init__.py
@@ -1,7 +1,7 @@
 from .core import SimpleJsoner
 
-jsoner = SimpleJsoner
+
 read = SimpleJsoner.read
 write = SimpleJsoner.write
 
-__all__ = ["SimpleJsoner", "read", "write", "jsoner"]
+__all__ = ["SimpleJsoner", "read", "write"]
